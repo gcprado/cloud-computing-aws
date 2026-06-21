2c1fe02f6950e3a9c10909aac1c9bfb0370da5bb
Giancarlo Prado <giancarlo.prado101@gmail.com>
2026-06-21 16:52:04 +0100

    fix typo

--- backend/ecs/src/app.js
+++ backend/ecs/src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const swaggerUi = require("swagger-ui-express");
 
 const swaggerSpec = require("./docs/swagger");
 
@@ -25,19 +24,42 @@ app.use((req, res, next) => {
 app.use("/health", healthRoutes);
 app.use("/items", itemsRoutes);
 
-const swaggerHtml = swaggerUi.generateHTML(swaggerSpec, {
-  customCss: '.swagger-ui .topbar { display: none }'
-});
+const getSwaggerHtml = () => `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inventory ECS API - Documentation</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.10.0/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5.10.0/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist@5.10.0/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = function() {
+      const ui = SwaggerUIBundle({
+        spec: ${JSON.stringify(swaggerSpec)},
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        layout: "StandaloneLayout"
+      });
+    };
+  </script>
+</body>
+</html>`;
 
 app.get("/docs", (req, res) => {
-  res.send(swaggerHtml);
+  res.send(getSwaggerHtml());
 });
 
 app.get("/docs/", (req, res) => {
-  res.send(swaggerHtml);
+  res.send(getSwaggerHtml());
 });
-
-app.use("/docs", swaggerUi.serveFiles(swaggerSpec, {}));
 
 app.get("/openapi.json", (req, res) => {
   res.json(swaggerSpec);
